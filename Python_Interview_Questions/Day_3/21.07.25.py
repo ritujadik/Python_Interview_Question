229a85e7cf517d4a133daf570cf1a0c36dbe2f3c
Rituja Dikshit <144586025+ritujadik@users.noreply.github.com>
2025-07-22 01:03:03 +0530

    Daily Python interview question day-3

---
+++ Python_Interview_Questions/Day_3/21.07.25.py
@@ -0,0 +1,76 @@
+#Ques-1
+# what are the different datat types in python?
+# text-string
+#number-int,float,complex number
+#sequence-list,tuple,range
+#set-set,frozenset
+#mapping-dict
+#binary-byte,bytearray,memoryview
+#None-none
+
+#Ques-2
+# How do you iterate through a list in python?
+#Answer: x = [1,2,3]
+#        for i in x:
+#           print(i)
+# by list comprehnsion
+#         print([i: i for i in x])
+
+#Ques-3
+# what is dictionary and how do you access its elements
+#Answer dictionary is the mapping type of data type which can store the item in key value pair
+#example  stationery = {'Pencil':2,'Rubber':3}
+#access the element
+# print(stationery[pencil])
+# output would be 2
+# if we want to get the key of the dictionary
+#print(stationery.keys()
+#output = Pencil,Rubber
+
+#Ques-4
+#Explain the concept of conditional statement in python?
+#Ans: when we have condition and we want the response accordingly
+# like if,else,elif condition to satisfy those condition and response on that basis
+#Example if the person is greater than 18 than he/she can vote else they dont
+# if age >= 18:
+#   print("he is allowed to vote)
+# else:
+#   print("he is not allowed to vote)
+
+#Ques-5 what are the function and how do you define them
+#Ans-function are the block of code which we can write once but we can reuse the same whenever needed.
+# like def sum(a,b):
+#       sum = a + b
+#       return sum
+# a = 2,b = 3
+# print(sum(a,b))
+
+#Ques-6 How do you handle error and exception in python
+#Answer: we can write the code in the try and except block where in try block we can write the whole code which wants to execute and
+# in except we can raise the error if something does not execute
+
+# Ques-7 what does the garbage collector do
+#Answer: garbage collection automatically allocate and deallocate the memory in python
+
+# Ques-8 how do you import external libraries in python
+# Answer by using import method
+# import math
+# import random
+
+# Ques-9 Explain the difference between mutuable and immutuable object in python?
+#Mutuable object are those on that we can perform the CRUD operation like create,read,update and delete
+# ex- list,dict
+# immutable object are those on that we can do read operation and not able to perform CRUD operation
+# ex-set,tuple
+#Ques-10- what is the 'in' operator in python?
+#Answer-in operator basically check the element exist in the data set or not
+# example- x = 1,2,3
+#          a = 2
+#          if a in x:
+#            print(f"Yes {a}does exist in {x})
+
+# Ques-11-How we can write the docstring in python
+#Answer-we can use triple quoted string to write the functions,explaining their purpose,arguments, and return values
+
+
+#Ques-5
