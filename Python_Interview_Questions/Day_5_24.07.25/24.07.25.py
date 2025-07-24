2b372b1222c96541ab8a0aab51f99137ddb62784
Rituja Dikshit <144586025+ritujadik@users.noreply.github.com>
2025-07-24 23:52:55 +0530

    Python 10 Daily Interview Question Series Day-6

---
+++ Python_Interview_Questions/Day_5_24.07.25/24.07.25.py
@@ -0,0 +1,12 @@
+# Ques-1-Is python complied or interpreted language
+# Ques-2 What is the difference between '/' and '//'
+# Ques-3 In python we use call by reference or call by value
+# Ques-4 what is floor value in python
+# Ques-5 what is dynamically typed language
+# Ques-6 can we take one function as an argument in another function
+# Ques-7 Is indentation required in python
+# Ques-8 what is the use of pass keyword in python
+# Ques-9 How we concatenate two list in python
+# Ques-10 what is the lambda function
+
+""" We made the video on the above questions"""
