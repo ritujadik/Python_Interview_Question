492b5ae0c0f8371e3f77c23a274d8f3cfb0f0f58
Rituja Dikshit <144586025+ritujadik@users.noreply.github.com>
2025-07-28 19:35:45 +0530

    Daily Interview Question series

---
+++ Python_Interview_Questions/Day_7_28.07.25/28.07.25.py
@@ -0,0 +1,10 @@
+""""Interview questions realted to the moneky patching topic """
+"""
+Question-1 what is monkey patching and how we use?
+Question-2 how monkey patching and decorators are different from each other?
+Question-3 what are the risk of monkey patching?
+Question-4 when would you use monkey patching in python?
+Question-5 Is monkey patching considered as a good practise?why or whynot
+Question-6 what is the inbuilt type of monkey patch
+Question-7 Monkey patch a method to log calls to it
+"""
